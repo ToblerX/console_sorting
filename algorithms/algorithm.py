c2208b7bbb975e911dc5bf22ed6120d1f5b1b65b
ToblerX <super.yaroslav05@gmail.com>
2025-04-05 15:20:52 +0200

    typing fixes

--- algorithms/algorithm.py
+++ algorithms/algorithm.py
@@ -1,7 +1,10 @@
+from typing import List, Tuple, Optional
+
 class Algo(object):
-    def __init__(self, arr: list, count_time: bool) -> None:
+    def __init__(self, arr: List[int], count_time: bool) -> None:
         self.arr = arr
         self.count_time = count_time
-    def run(self, arr: list, count_time: bool) -> list: #not list but Tuple, because of static order of elements of different type
-        """Sort the array using algorithm X"""
+
+    def run(self, arr: List[int], count_time: bool) -> Tuple[List[int], Optional[float]]:
+        """Sort the array using algorithm X, return (sorted list, time or None)"""
         pass
--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,16 +1,27 @@
 from algorithm import Algo
+from typing import List, Tuple, Union
+import time
+
 
 class BubbleSort(Algo):
     def __init__(self, arr, count_time):
         super().__init__(arr, count_time)
-    def run(self, arr: list, count_time: bool):
+
+    def run(self, arr: list, count_time: bool) -> Union[List[int], Tuple[List[int], float]]:
+        if count_time:
+            start = time.time()
+
         n = len(arr)
         for i in range(n):
             swapped = False
-            for j in range(n-i-1):
-                if arr[j] > arr[j+1]:
-                    arr[j], arr[j+1] = arr[j+1], arr[j]
+            for j in range(n - i - 1):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
                     swapped = True
             if not swapped:
                 break
-        return arr #implement a timer and also return elapsed time if count_time=True
+
+        if count_time:
+            end = time.time()
+            return arr, end - start
+        return arr
